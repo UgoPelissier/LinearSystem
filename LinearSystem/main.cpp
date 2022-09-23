2d490cc3dceab6648cd9a73f3f53715ca83679a8
Ugo Pelissier <upelissier@gmail.com>
2022-09-23 17:57:58 +0200

    Initial Commit

---
+++ LinearSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinearSystem
+//
+//  Created by Famille PELISSIER on 23/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
